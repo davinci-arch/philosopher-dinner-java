65cc3be045bdf0586654b14a6273feb0cf41bae6
parfesa.a15@gmail.com <parfesa.a15@gmail.com>
2024-03-17 18:31:18 +0200

    [fetch]: philospher cannot take an fork if other four philosopher alreayd took

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,13 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
     public static void main(String[] args) {
-
-        for (int i = 0; i < 5; i++) {
-            new Philosopher(i).start();
+        Semaphore[] forks = new Semaphore[5];
+        Semaphore permissionToTake = new Semaphore(4);
+        for (int i = 0; i < forks.length; i++) {
+            forks[i] = new Semaphore(1);
+        }
+        for (int i = 1; i <= 5; i++) {
+            new Philosopher(i, permissionToTake, forks).start();
         }
 
 
--- src/main/java/org/example/Philosopher.java
+++ src/main/java/org/example/Philosopher.java
@@ -7,45 +7,40 @@ public class Philosopher extends Thread {
     private final int id;
     private int idLeftFork;
     private int idRightFork;
+    private Semaphore permissionToTake;
     private Semaphore[] forks;
 
-    public Philosopher(int id) {
+    public Philosopher(int id, Semaphore permissionToTake, Semaphore[] forks) {
         this.id = id;
-        idLeftFork = id;
-        idRightFork = id % 5;
-        forks = getForks();
-    }
-
-    private static Semaphore[] getForks() {
-        Semaphore[] forks = new Semaphore[5];
-
-        for (int i = 0; i < 5; i++) {
-            forks[i] = new Semaphore(1);
-        }
-
-        return forks;
+        idLeftFork = ((id % 5) + 1) - 1;
+        idRightFork = id - 1;
+        this.permissionToTake = permissionToTake;
+        this.forks = forks;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
+
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Phylosopher " + id + " thinking " + i + " times");
 
             try {
+
+                permissionToTake.acquire();
+
                 forks[idLeftFork].acquire();
-                System.out.println("Phylosopher " + id + " took left fork");
-
+                System.out.println("Phylosopher " + id + " took left fork with id: " + idLeftFork);
                 forks[idRightFork].acquire();
-                System.out.println("Phylosopher " + id + " took right fork");
-
+                System.out.println("Phylosopher " + id + " took right fork with id: " + idRightFork);
 
                 System.out.println("Phylosopher " + id + " eating " + i + " times");
 
                 forks[idRightFork].release();
                 System.out.println("Phylosopher " + id + " put right fork");
-
                 forks[idLeftFork].release();
                 System.out.println("Phylosopher " + id + " put left fork");
+
+                permissionToTake.release();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
